a42fb5822c2a434a98f7006c7e344cf305d7ad20
KanaKarimata <kana.lt22@gmail.com>
2022-11-21 08:53:10 +0900

    add plugin vuex

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    // state管理
+    // どういうデータを管理するのか
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+    // 非同期通信で使う
+  },
+  modules: {
+  }
+})
