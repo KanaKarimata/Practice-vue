69c174f1d09b475e87d97bf1981700f8a5fe890f
KanaKarimata <kana.lt22@gmail.com>
2022-11-21 09:09:14 +0900

    create save method

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,16 @@ export default createStore({
   state: {
     // state管理
     // どういうデータを管理するのか
+    memos: []
   },
   getters: {
+    // データの取得の方法
   },
   mutations: {
+    // save memo
+    save (state, newMemo) {
+      state.memos.unshift(newMemo)
+    }
   },
   actions: {
     // 非同期通信で使う
